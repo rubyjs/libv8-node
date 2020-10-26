b83d5d1c242d2c21899ad632e24db4c65492bf0d
Loic Nageleisen <loic@sqreen.io>
2020-10-26 10:28:36 +0100

    Build node 14.14.0

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = "12.18.4.0.beta1"
-  NODE_VERSION = "12.18.4"
-  LIBV8_VERSION = "7.8.279.23" # v8/include/v8-version.h
+  VERSION = "14.14.0.0.beta1"
+  NODE_VERSION = "14.14.0"
+  LIBV8_VERSION = "8.4.371.19" # v8/include/v8-version.h
 end
