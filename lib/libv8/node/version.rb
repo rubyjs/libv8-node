b369cffbd1f22e7097dbf60ab7e794fa2400e3af
Loic Nageleisen <loic@sqreen.io>
2020-10-30 18:15:38 +0100

    Release 15.0.1.0.beta2

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = "15.0.1.0.beta1"
+  VERSION = "15.0.1.0.beta2"
   NODE_VERSION = "15.0.1"
   LIBV8_VERSION = "8.6.395.17" # v8/include/v8-version.h
 end
