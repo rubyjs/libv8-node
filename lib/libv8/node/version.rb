5a69d5c17af82d7877eaecd2609a2c3da9743305
Loic Nageleisen <loic@sqreen.com>
2021-03-30 17:23:29 +0200

    Build from node 15.12.0

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = '15.9.0.0.beta1'.freeze
-  NODE_VERSION = '15.9.0'.freeze
+  VERSION = '15.12.0.0.beta1'.freeze
+  NODE_VERSION = '15.12.0'.freeze
   LIBV8_VERSION = '8.6.395.17'.freeze # from v8/include/v8-version.h
 end
