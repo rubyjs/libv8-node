95e846e64596ef4670dc378190d0788135f28a18
Loic Nageleisen <loic@sqreen.com>
2021-10-16 00:21:51 +0200

    Downgrade to node 16.5.0

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = '16.6.2.0'.freeze
-  NODE_VERSION = '16.6.2'.freeze
-  LIBV8_VERSION = '9.2.230.21'.freeze # from v8/include/v8-version.h
+  VERSION = '16.5.0.0'.freeze
+  NODE_VERSION = '16.5.0'.freeze
+  LIBV8_VERSION = '9.1.269.38'.freeze # from v8/include/v8-version.h
 end
