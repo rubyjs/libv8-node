f53bf1e104c88cbfaf7cca99edd4aa2d1e3a94c7
Loic Nageleisen <loic.nageleisen@gmail.com>
2022-08-26 21:18:31 +0200

    Update to node 16.17.0

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = '16.10.0.0'.freeze
-  NODE_VERSION = '16.10.0'.freeze
-  LIBV8_VERSION = '9.3.345.19'.freeze # from v8/include/v8-version.h
+  VERSION = '16.17.0.0'.freeze
+  NODE_VERSION = '16.17.0'.freeze
+  LIBV8_VERSION = '9.4.146.26'.freeze # from v8/include/v8-version.h
 end
