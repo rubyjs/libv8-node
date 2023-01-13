5caeb09ac856f1b1d07134af0f6ee7d2aee94fa2
Sean Mann <sean@drizzlybear.com>
2023-01-12 21:56:30 -0800

    get test to reference built libv8

--- test/gtest/hello_test.cc
+++ test/gtest/hello_test.cc
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
 
+#include <v8.h>
+#include <libplatform/libplatform.h>
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
+  std::shared_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
