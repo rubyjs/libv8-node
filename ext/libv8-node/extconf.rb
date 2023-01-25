c0eb5030ba00b8d5bb1f0b743a46947b29c3e251
Loic Nageleisen <loic.nageleisen@gmail.com>
2023-01-25 03:08:58 +0100

    Fix and bump Rubocop

--- ext/libv8-node/extconf.rb
+++ ext/libv8-node/extconf.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require 'mkmf'
+
+if RUBY_ENGINE == 'truffleruby'
+  File.write('Makefile', dummy_makefile($srcdir).join('')) # rubocop:disable Style/GlobalVars
+  return
+end
+
 create_makefile('libv8-node')
 
 require File.expand_path('location', __dir__)
