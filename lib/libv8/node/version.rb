65e14af1d1ca7583c1e6a35c7ad73e90b73cefa4
Loic Nageleisen <loic.nageleisen@gmail.com>
2023-05-23 20:24:46 +0200

    Update to node 20.2.0

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = '19.9.0.0'.freeze
-  NODE_VERSION = '19.9.0'.freeze
-  LIBV8_VERSION = '10.8.168.25'.freeze # from v8/include/v8-version.h
+  VERSION = '20.2.0.0'.freeze
+  NODE_VERSION = '20.2.0'.freeze
+  LIBV8_VERSION = '11.3.244.8'.freeze # from v8/include/v8-version.h
 end
