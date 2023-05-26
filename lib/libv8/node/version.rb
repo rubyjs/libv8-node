0286ddef47565e1b5a6ccb54780613e06ca9597a
Loic Nageleisen <loic.nageleisen@gmail.com>
2023-05-26 11:59:11 +0200

    Release 18.16.0.0

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = '18.13.0.1'.freeze
-  NODE_VERSION = '18.13.0'.freeze
-  LIBV8_VERSION = '10.2.154.23'.freeze # from v8/include/v8-version.h
+  VERSION = '18.16.0.0'.freeze
+  NODE_VERSION = '18.16.0'.freeze
+  LIBV8_VERSION = '10.2.154.26'.freeze # from v8/include/v8-version.h
 end
