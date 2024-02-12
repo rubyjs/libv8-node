21cd9dba381b68c4edd19eee31876b0f8033e827
Jarek Radosz <jradosz@gmail.com>
2024-02-12 21:58:51 +0100

    Release 18.19.0.0

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = '18.16.0.0'.freeze
-  NODE_VERSION = '18.16.0'.freeze
+  VERSION = '18.19.0.0'.freeze
+  NODE_VERSION = '18.19.0'.freeze
   LIBV8_VERSION = '10.2.154.26'.freeze # from v8/include/v8-version.h
 end
