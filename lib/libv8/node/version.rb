e7e17d2a61e0065cf9d8f3c655ae524ea4fd26d6
Loic Nageleisen <loic.nageleisen@gmail.com>
2024-07-23 16:22:53 +0200

    Fix libv8 version

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -3,5 +3,5 @@ module Libv8; end
 module Libv8::Node
   VERSION = '22.5.1.0'.freeze
   NODE_VERSION = '22.5.1'.freeze
-  LIBV8_VERSION = '12.4.254.21'.freeze # from v8/include/v8-version.h
+  LIBV8_VERSION = '21.4.254.21'.freeze # from v8/include/v8-version.h
 end
