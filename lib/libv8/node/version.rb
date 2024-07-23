aae83f9d9c4d05c5a892267de4d6afea983c4396
Loic Nageleisen <loic.nageleisen@gmail.com>
2024-07-23 14:35:17 +0200

    Update to node 22.5.1

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,7 @@
 module Libv8; end
 
 module Libv8::Node
-  VERSION = '21.7.2.0'.freeze
-  NODE_VERSION = '21.7.2'.freeze
-  LIBV8_VERSION = '11.8.172.17'.freeze # from v8/include/v8-version.h
+  VERSION = '22.5.1.0'.freeze
+  NODE_VERSION = '22.5.1'.freeze
+  LIBV8_VERSION = '12.4.254.21'.freeze # from v8/include/v8-version.h
 end
