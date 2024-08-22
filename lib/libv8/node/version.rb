362241d2cc98e87e41da4bde57eec0953cdf9903
Sam Saffron <sam.saffron@gmail.com>
2024-08-22 12:20:36 +1000

    not sure why but we are putting the wrong version here.

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -6,5 +6,5 @@ end
 module Libv8::Node
   VERSION = "22.6.0.0"
   NODE_VERSION = "22.6.0"
-  LIBV8_VERSION = "12.4.254.21" # from src/node-.../deps/v8/include/v8-version.h
+  LIBV8_VERSION = "21.4.254.21" # from src/node-.../deps/v8/include/v8-version.h
 end
