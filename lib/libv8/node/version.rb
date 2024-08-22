a616a9a0a44e278c990c52c44a8321678701db95
Sam Saffron <sam.saffron@gmail.com>
2024-08-22 12:18:23 +1000

    bump version

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -1,7 +1,10 @@
-module Libv8; end
+# frozen_string_literal: true
+
+module Libv8
+end
 
 module Libv8::Node
-  VERSION = '22.5.1.0'.freeze
-  NODE_VERSION = '22.5.1'.freeze
-  LIBV8_VERSION = '21.4.254.21'.freeze # from v8/include/v8-version.h
+  VERSION = "22.6.0.0"
+  NODE_VERSION = "22.6.0"
+  LIBV8_VERSION = "12.4.254.21" # from src/node-.../deps/v8/include/v8-version.h
 end
