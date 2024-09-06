c424a54d3e7fe0e4ba26306513e6d7b6cdd167e0
Sam Saffron <sam.saffron@gmail.com>
2024-09-07 09:41:58 +1000

    bump version so we can build a new mini racer

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -4,7 +4,7 @@ module Libv8
 end
 
 module Libv8::Node
-  VERSION = '22.7.0.2'
+  VERSION = '22.7.0.3'
   NODE_VERSION = '22.7.0'
   LIBV8_VERSION = '12.4.254.21' # from src/node-.../deps/v8/include/v8-version.h
 end
