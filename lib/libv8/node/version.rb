b234a7155e187c41d82991a43380257c514ab1da
Ben Noordhuis <info@bnoordhuis.nl>
2025-01-30 22:38:33 +0100

    Update to node 23.6.1

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -4,7 +4,7 @@ module Libv8
 end
 
 module Libv8::Node
-  VERSION = '22.13.1.0'
-  NODE_VERSION = '22.13.1'
-  LIBV8_VERSION = '12.4.254.21' # from src/node-.../deps/v8/include/v8-version.h
+  VERSION = '23.6.1.0'
+  NODE_VERSION = '23.6.1'
+  LIBV8_VERSION = '12.9.202.28' # from src/node-.../deps/v8/include/v8-version.h
 end
