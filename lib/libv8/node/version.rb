2397d487bc6bbd5227db1adf97a52db706223e68
Sam Saffron <sam.saffron@gmail.com>
2025-02-17 15:24:17 +1100

    bump node version ... libv8 remains unchanged though

--- lib/libv8/node/version.rb
+++ lib/libv8/node/version.rb
@@ -4,7 +4,7 @@ module Libv8
 end
 
 module Libv8::Node
-  VERSION = '23.6.1.0'
-  NODE_VERSION = '23.6.1'
+  VERSION = '23.8.0.0'
+  NODE_VERSION = '23.8.0'
   LIBV8_VERSION = '12.9.202.28' # from src/node-.../deps/v8/include/v8-version.h
 end
